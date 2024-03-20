0113b3302422e39088ac05535d35e36172c38990
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-20 20:03:45 -0300

    Revert "role_user migration and model"
    This reverts commit 6d15d35e0c9ba59d2eb679fd9d7c7169c432df11.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,34 +2,46 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
+        'email',
+        'password',
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
      */
-    protected $casts = [
-        'id' => 'integer',
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 
-    public function roles(): BelongsToMany
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->belongsToMany(Role::class);
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
