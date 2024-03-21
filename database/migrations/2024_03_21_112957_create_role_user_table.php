aa898eb93140fd048648b183da04063c490ef103
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-21 08:36:42 -0300

    Update 2024_03_21_112957_create_role_user_table.php

--- database/migrations/2024_03_21_112957_create_role_user_table.php
+++ database/migrations/2024_03_21_112957_create_role_user_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unique(['role_id', 'user_id']);
             $table->timestamps();
         });
     }
