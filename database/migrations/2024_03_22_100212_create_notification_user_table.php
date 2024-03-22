03f6b6dc6ca10871fc4126c10d66f6241c032bdd
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-22 07:17:05 -0300

    Update 2024_03_22_100212_create_notification_user_table.php

--- database/migrations/2024_03_22_100212_create_notification_user_table.php
+++ database/migrations/2024_03_22_100212_create_notification_user_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('notification_user', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('notification_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('is_read')->default(false);
+            $table->foreign('notification_id')->references('id')->on('notifications');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
