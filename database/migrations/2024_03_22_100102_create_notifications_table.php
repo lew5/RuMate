42280dc9067aefeffbac16762e818bd01f709189
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-22 07:11:24 -0300

    Update 2024_03_22_100102_create_notifications_table.php

--- database/migrations/2024_03_22_100102_create_notifications_table.php
+++ database/migrations/2024_03_22_100102_create_notifications_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->unique();
+            $table->string('title')->unique();
+            $table->text('message');
             $table->timestamps();
         });
     }
