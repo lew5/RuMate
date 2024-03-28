7ea3d8a0685bedd677aeb697dcc6f926adb8194a
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-28 09:42:58 -0300

    feat: Update Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string',
+    ];
 }
