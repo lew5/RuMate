edba3bc152bd5cc1085ab6ea1c0d247fbeb49f57
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-29 10:47:29 -0300

    feat: Add relationship between Notification and User

---
+++ app/Models/Notification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'title',
+        'message'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'type' => 'string',
+            'title' => 'string',
+            'message' => 'string'
+        ];
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    public function notifications(): BelongsToMany
+    {
+        return $this->belongsToMany(Notification::class);
+    }
 }
