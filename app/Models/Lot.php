31b80afe3b78bf8bdae302aa3834b6f1b016022f
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-30 10:43:39 -0300

    feat: Add Lot $fillable and casts(

--- app/Models/Lot.php
+++ app/Models/Lot.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Lot extends Model
 {
     use HasFactory;
+
+    protected $fillable  = ['name', 'starting_price', 'status', 'winner_user_id'];
+
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'starting_price' => 'float',
+            'status' => 'string',
+            'winner_user_id' => 'int',
+        ];
+    }
 }
