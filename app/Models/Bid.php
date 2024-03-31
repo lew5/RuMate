945b4e3116dcf82e1c9334ac718e8c6dfd172701
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-31 11:46:31 -0300

    feat: Add relationship between Bid and User

--- app/Models/Bid.php
+++ app/Models/Bid.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bid extends Model
 {
@@ -14,5 +15,10 @@ class Bid extends Model
     protected function casts(): array
     {
         return ['amount' => 'float'];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
