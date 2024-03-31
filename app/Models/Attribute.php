cc66ebbaf002fcd9067277c557addabddc821803
Leandro <53401001+lew5@users.noreply.github.com>
2024-03-31 10:57:26 -0300

    feat: Add relationship between Attribute and Category

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Attribute extends Model
 {
@@ -14,5 +15,10 @@ class Attribute extends Model
     protected function casts(): array
     {
         return ['name' => 'string'];
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
@@ -14,5 +15,10 @@ class Category extends Model
     protected function casts(): array
     {
         return ['name' => 'string'];
+    }
+
+    public function attributes(): BelongsToMany
+    {
+        return $this->belongsToMany(Attribute::class);
     }
 }
