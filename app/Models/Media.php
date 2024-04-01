72757e2fa5d7e3f4075bf48d84ad4eb22b06c2f1
Leandro <53401001+lew5@users.noreply.github.com>
2024-04-01 08:58:49 -0300

    feat: Add Media $fillable and casts()

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Media extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['mediable_id', 'mediable_type', 'url', 'type'];
+
+    protected function casts(): array
+    {
+        return ['url' => 'string', 'type' => 'string'];
+    }
 }
