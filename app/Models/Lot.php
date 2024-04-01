48dc3d9309f5028c329724f15647b664fafa2a3c
Leandro <53401001+lew5@users.noreply.github.com>
2024-04-01 10:20:43 -0300

    feat: Add relationship between Lot and Media

--- app/Models/Lot.php
+++ app/Models/Lot.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Lot extends Model
 {
@@ -46,5 +47,10 @@ class Lot extends Model
     public function bids(): HasMany
     {
         return $this->hasMany(Bid::class);
+    }
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'mediable');
     }
 }
