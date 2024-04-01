0ce97479323481f0280d9f09511c415cd67a9fce
Leandro <53401001+lew5@users.noreply.github.com>
2024-04-01 08:51:42 -0300

    feat: Add UserDetail $fillable and casts()

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class UserDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'address', 'phone'];
+
+    protected function casts(): array
+    {
+        return ['address' => 'string', 'phone' => 'string'];
+    }
 }
